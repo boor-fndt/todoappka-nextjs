b8312d3e4feef15906c2aaceb4c48a17058fcca1
boor <boor@foundation.sk>
2023-11-22 11:06:09 +0100

    github actions wip

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,13 @@
 
 cd /home/ubuntu/todoappka-nextjs
 
-# Aktualizujte kód zo vzdialeného repozitára (GitHub)
 git pull origin main
 
-# Nainštalujte alebo aktualizujte závislosti
 npm install
 
-# Zastavte existujúci proces Next.js
 pm2 stop all
 pm2 delete all
 
 npm run build
 
-# Spustite Next.js aplikáciu cez PM2
 pm2 start npm --name nextjs-app -- run start -- -p 3000
